b9cc464d19ccdfea8ceb3d2843a7b1bd1575b377
Sidharth sangelia <sidharthsangelia@gmail.com>
2025-07-12 00:31:05 +0530

    diabled eslint during build

--- components/EnhancedFormInput.tsx
+++ components/EnhancedFormInput.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Slider } from "@/components/ui/slider";
 import { getFormData } from '@/actions/getFormData';
